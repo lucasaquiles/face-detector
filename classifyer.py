b0ddbc0ed4c122e83d34fdbc4b88bbf2276a5328
Lucas Aquiles <lucasaquiles@gmail.com>
2019-04-15 13:49:40 -0300

    update destaque das partes do rosto

--- classifyer.py
+++ classifyer.py
@@ -10,14 +10,14 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.2, 5)
 for (x,y,w,h) in faces:
-    # cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
 
-    # eyes = eye_cascade.detectMultiScale(roi_gray)
-    # print eyes
-    # for (ex,ey,ew,eh) in eyes:
-    #     cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    print eyes
+    for (ex,ey,ew,eh) in eyes:
+        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     smile = smile_cascade.detectMultiScale(roi_gray)
     for (sx,sy,sw,sh) in smile:
     	 cv.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(1,0,0),1)
