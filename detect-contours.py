b995a39c45a77d14942b5949c42abe83f9de37af
Lucas Aquiles <lucasaquiles@gmail.com>
2019-04-24 18:13:47 -0300

    retorno da imagem

--- detect-contours.py
+++ detect-contours.py
@@ -8,16 +8,20 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 img = cv2.imread('sachin.jpg')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+faces = eye_cascade.detectMultiScale(gray, 1.2, 5)
 
 for (x,y,w,h) in faces: 
       
         roi_gray = gray[y:y+h, x:x+w] 
         roi_color = img[y:y+h, x:x+w] 
-        ret, thresh = cv2.threshold(gray, 127, 255, 1)
-        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(img, contours, -1, (0,0,0), 1)
 
-cv2.imshow('img', img)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        
+        ret, thresh = cv2.threshold(gray, 128, 255, 1)
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print im2
+        cv2.drawContours(im2, contours, -2, (0,0,0), 1)
+
+cv2.imshow('img', im2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
